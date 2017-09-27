e03d19dba5b9ff48e89cfca40c3147906a481569
Carl Hofmeister <hofmeister.carl@gmail.com>
2017-09-26 22:03:15 -0600

    Added load instruction

--- cpu.py
+++ cpu.py
@@ -5,12 +5,13 @@ class Micro(object):
 
     def __init__(self):
         ''' Initialize registers to 0'''
-        self._x0     = 0
-        self._x1     = 0
-        self._y0     = 0
-        self._y1     = 0
-        self._i      = 0
-        self._m      = 0
+        self._x0    = 0
+        self._x1    = 0
+        self._y0    = 0
+        self._y1    = 0
+        self._i     = 0
+        self._m     = 0
+        self._r     = 0
         self.o_reg  = 0
         self.i_pins = 0
         self._dm = [0]*16
@@ -19,8 +20,31 @@ class Micro(object):
         ''' String representation of the mircoprocessor state'''
         registers = 'Registers:\n'
         registers += f'x0:{self._x0}   x1:{self._x1}   y0:{self._y0}   y1:{self._y1}\n'
-        registers += f'i:{self._i}   m:{self._m}   o_reg:{self.o_reg}  i_pins:{self.i_pins}\n'
+        registers += f'i:{self._i}   m:{self._m}   r:{self._r}  o_reg:{self.o_reg}  i_pins:{self.i_pins}\n'
         memory = 'Memory:\n'
         memory += str(self._dm)
         return registers+memory
+
+
+    def load(self, dest, data):
+        ''' Load a valid register with a value'''
+        if dest == 'x0':
+            self._x0 = data
+        elif dest == 'x1':
+            self._x1 = data
+        elif dest == 'y0':
+            self._y0 = data
+        elif dest == 'y1':
+            self._y1 = data
+        elif dest == 'i':
+            self._i = data
+        elif dest == 'm':
+            self._m = data
+        elif dest == 'o_reg':
+            self.o_reg = data
+        elif dest == 'dm':
+            self._dm[self._i] = data
+        else:
+            raise ValueError('Invalid destination register')
+
 
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from cpu import Micro
 
+registers = ['x0', 'x1', 'y0', 'y1', 'o_reg', 'i_pins', 'i', 'm', 'r']
+int_registers = ['_x0', '_x1', '_y0', '_y1', 'o_reg', 'i_pins', '_i', '_m', '_r']
 
 def test_init():
     m = Micro()
@@ -9,6 +11,7 @@ def test_init():
     assert(m._y1 == 0)
     assert(m._i == 0)
     assert(m._m == 0)
+    assert(m._r == 0)
     assert(m.i_pins == 0)
     assert(m.o_reg == 0)
     assert(m._dm == [0]*16)
@@ -19,7 +22,34 @@ def test_string():
     expected =\
 '''Registers:
 x0:0   x1:0   y0:0   y1:0
-i:0   m:0   o_reg:0  i_pins:0
+i:0   m:0   r:0  o_reg:0  i_pins:0
 Memory:
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]'''
     assert(str(m) == expected)
+
+def test_load():
+    m = Micro()
+    def check_load(reg, data):
+        m.load(reg, data)
+        if reg != 'o_reg':
+            reg_str = '_'+reg
+        else:
+            reg_str = reg
+        assert(getattr(m, reg_str) == data)
+
+    # Test valid registers
+    # TODO: use random numbers for data
+    for reg in registers:
+        if reg not in ['i_pins', 'r']:
+            check_load(reg, 1)
+
+    # Test RAM
+    # TODO: make this more thurough
+    m.load('dm', 1)
+    assert(m._dm[m._i] == 1)
+
+    try:
+        m.load('i_pins', 1)
+        raise RuntimeError('Did not handle invalid destination')
+    except ValueError:
+        pass
